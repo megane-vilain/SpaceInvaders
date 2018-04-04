a0092fe409b21fb91234c7e30e9cfbcb3775b616
vandulya <38074183+vandulya@users.noreply.github.com>
2018-04-04 15:05:13 +0200

    Futur fonction de tir

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ while 1:
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT and shiprect.right < width:
             shiprect = shiprect.move(speed,0)
+		
+		if event.type == pygame.KEYDOWN and event.key == pygame.K_UP and shiprect.right < width:
+            pass
+			#futur fonction de tir
+		
 
     screen.fill(white)
     screen.blit(ship, shiprect)
